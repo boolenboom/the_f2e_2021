340390fdf1806cc79d20c353ed8d3592b21efea9
boolenboom <alan93072@gmail.com>
2021-12-08 23:55:25 +0800

    List 套 router-link

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,8 +1,10 @@
 <template>
     <div class="theme-list">
         <ul class="d-grid container">
-            <li v-for="cardData of cardsData" :key='cardData.id'>
-                <slot name='items' :cardInfo='cardData'></slot>
+            <li v-for="cardData of cardsData" :key='cardData.ID'>
+                <router-link :to="`/content/${$route.params.category}/${cardData.ID}`">
+                    <slot name='items' :cardInfo='cardData'></slot>
+                </router-link>
             </li>
         </ul>
     </div>

4c21c124417eaf3f3a4a442308b3cfe388afb727
boolenboom <alan93072@gmail.com>
2021-12-17 20:53:34 +0800

    file hash array 新增圖片hash

--- src/assets/filenameHashList.js
+++ src/assets/filenameHashList.js
@@ -7,13 +7,15 @@ import m_home_banner3 from '@/assets/hero/m_home_banner3.jpg';
 import activity_banner from '@/assets/sub_hero/activity_banner.jpg';
 import restaurant_banner from '@/assets/sub_hero/restaurant_banner.jpg';
 import view_banner from '@/assets/sub_hero/view_banner.jpg';
+import search_banner from '@/assets/sub_hero/search_banner.jpg';
 import m_activity_banner from '@/assets/sub_hero/m_activity_banner.png';
 import m_restaurant_banner from '@/assets/sub_hero/m_restaurant_banner.png';
 import m_view_banner from '@/assets/sub_hero/m_view_banner.png';
+import m_search_banner from '@/assets/sub_hero/m_search_banner.png';
 let heroImgs = [home_banner,home_banner2,home_banner3,
     m_home_banner,m_home_banner2,m_home_banner3];
-let subHeroImgs = [view_banner,restaurant_banner,activity_banner,
-    m_view_banner,m_restaurant_banner,m_activity_banner,];
+let subHeroImgs = [view_banner,restaurant_banner,activity_banner,search_banner,
+    m_view_banner,m_restaurant_banner,m_activity_banner,m_search_banner];
 export {
     heroImgs,
     subHeroImgs

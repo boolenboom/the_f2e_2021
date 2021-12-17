85660b8381a7dd5e98ac26f4c9bfef62e35ff973
boolenboom <alan93072@gmail.com>
2021-12-17 20:57:51 +0800

    新增卡片判斷職責 & 無資料顯示

--- src/components/List.vue
+++ src/components/List.vue
@@ -2,11 +2,27 @@
     <div class="theme-list">
         <ul class="d-grid container">
             <li v-for="cardData of cardsData" :key='cardData.ID'>
-                <router-link :to="`/content/${$route.params.category}/${cardData.ID}`">
-                    <slot name='items' :cardInfo='cardData'></slot>
+                <router-link :to="`/content/${cardData.category}/${cardData.ID}`">
+                    <scenicspotcard
+                    v-if="cardData.category === 'ScenicSpot'"
+                    :cardInfo="cardData"
+                    />
+                    <activitycard
+                    v-if="cardData.category === 'Activity'"
+                    :cardInfo="cardData"
+                    />
+                    <foodcard
+                    v-if="cardData.category === 'Restaurant'"
+                    :cardInfo="cardData"
+                    />
                 </router-link>
             </li>
         </ul>
+        <div v-if="cardsData.length === 0" class="noResult">
+            <img src="../assets/bg/img_no result.png" alt="no result" srcset="">
+            <h2>搜尋不到資料</h2>
+            <h5>請改用其他關鍵字試試</h5>
+        </div>
     </div>
 </template>
 <style lang="scss">
@@ -20,11 +36,27 @@
             text-decoration: none;
         }
     }
+    .noResult{
+        h2{
+            color: #ff6000;
+        }
+        h5{
+            color: #a5a5a5;
+        }
+    }
 }
 </style>
 <script>
+import activitycard from "@/components/Activity_Card.vue";
+import scenicspotcard from "@/components/Scenicspot_Card.vue";
+import foodcard from "@/components/Food_Card.vue";
 export default {
     name:'List',
+    components:{
+        activitycard,
+        scenicspotcard,
+        foodcard,
+    },
     props:{
         cardsData:{type:Array}
     }

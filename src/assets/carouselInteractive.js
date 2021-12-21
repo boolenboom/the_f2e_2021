2e0fb30cf015bdf1a2257a105ba070f5034cb5a2
boolenboom <alan93072@gmail.com>
2021-12-21 16:36:42 +0800

    調整carousel 滑動速度 & 使函數保持單一職責 & 新增 checkIsSliding 方法

--- src/assets/carouselInteractive.js
+++ src/assets/carouselInteractive.js
@@ -1,20 +1,28 @@
 ;let handler=function(){
-    let isActive=false;
-    let startPoint=0,initial=0;
-    let inputRatio=0.005;
+    let isActive=false,isSlide=false;
+    let initial=0,startPoint=0,lastPoint=0;
+    let inputRatio=0.04;
     let isClamp=true,clampMin=0,clampMax=5;
     function evalOffset(offset){
         return isClamp ? Math.min(clampMax,Math.max(initial - (offset)*inputRatio, clampMin)) + 1 : initial - (offset)*inputRatio + 1;
     }
-    function endBehavior(endPoint){
-        isActive=false;
-        // console.log('test');
+    function evalEndTransform(endPoint){
         return Math.round(evalOffset(endPoint - startPoint));
     };
     return {
         pointermove:function(moveoffset){
-            if(isActive) return evalOffset(moveoffset -startPoint);
-            return false;
+            if(isActive){
+                lastPoint=moveoffset;
+                isSlide=true;
+            }
+            return isActive ? evalOffset(moveoffset -startPoint) : false;
+        },
+        touchmove:function(moveoffset){
+            if(isActive){
+                lastPoint=moveoffset;
+                isSlide=true;
+            }
+            return isActive ? evalOffset(moveoffset -startPoint) : false;
         },
         pointerdown:function(moveStart,currentInitial){
             isActive = true;
@@ -23,22 +31,31 @@
             return false;
         },
         pointerup:function(moveEnd){
-            return endBehavior(moveEnd);
+            isActive=false;
+            isSlide=false;
+            return evalEndTransform(moveEnd);
         },
         pointerout:function(moveEnd){
-            if(isActive) return endBehavior(moveEnd);
+            if(isActive) return evalEndTransform(moveEnd);
             return false;
         },
-        setClampOption:function(clampSwitch=isClamp,min=clampMin,max=clampMax){
+        touchend:function(moveEnd){
+            isActive=false;
+            isSlide=false;
+            return evalEndTransform(moveEnd || lastPoint);
+        },
+        setClampOption:function(clampSwitch=isClamp, min=clampMin, max=clampMax){
             isClamp = clampSwitch;
             clampMin = min;
             clampMax = max;
         },
         getClampOption:function(){
-            
             console.log('isClamp:'+isClamp);
             console.log('min:'+clampMin);
             console.log('max:'+clampMax);
+        },
+        checkIsSliding:function(){
+            return isSlide;
         }
     }
 }
